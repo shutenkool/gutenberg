627f0cd364f3cc625dcffb2184a97739bfbb31f6
Jorge <jorge.costa@developer.pt>
2019-04-19 12:53:34 +0100

    Connect gutenberg widget screen to widget-area endpoints.

---
+++ packages/edit-widgets/src/components/edit-widgets-initializer/index.js
@@ -0,0 +1,37 @@
+/**
+ * WordPress dependencies
+ */
+import { compose } from '@wordpress/compose';
+import { useEffect } from '@wordpress/element';
+import { withDispatch, withSelect } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import Layout from '../layout';
+
+function EditWidgetsInitializer( { widgetAreas, setupWidgetAreas } ) {
+	useEffect( () => {
+		if ( ! widgetAreas ) {
+			return;
+		}
+		console.log( widgetAreas );
+		setupWidgetAreas( widgetAreas );
+	}, [ widgetAreas, setupWidgetAreas ] );
+	return <Layout />;
+}
+
+export default compose( [
+	withSelect( ( select ) => {
+		const widgetAreas = select( 'core' ).getEntityRecords( 'root', 'widgetArea' );
+		return {
+			widgetAreas,
+		};
+	} ),
+	withDispatch( ( dispatch ) => {
+		const { setupWidgetAreas } = dispatch( 'core/edit-widgets' );
+		return {
+			setupWidgetAreas,
+		};
+	} ),
+] )( EditWidgetsInitializer );
